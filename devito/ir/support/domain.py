ec52e2ae3d1d2d2fd7a6881d7eae10cfb4e6b69d
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-01-03 17:24:02 +0100

    SQUASH WITH DOMAIN CONCEPTS

--- devito/ir/support/domain.py
+++ devito/ir/support/domain.py
@@ -8,7 +8,7 @@ from devito.tools import as_tuple
 class AbstractInterval(object):
 
     """
-    A representation of a closed interval on the set of integer numbers Z.
+    A representation of a closed interval on Z.
     """
 
     __metaclass__ = abc.ABCMeta
@@ -27,28 +27,28 @@ class AbstractInterval(object):
             partial = getattr(partial, key)(i)
         return partial
 
-    @abc.abstractproperty
-    def is_unbounded(self):
-        return
-
     @abc.abstractmethod
     def rebuild(self):
         return
 
     @abc.abstractmethod
-    def intersection(self, other):
+    def intersection(self, o):
         return
 
     @abc.abstractmethod
-    def union(self, other):
+    def union(self, o):
         return
 
     @abc.abstractmethod
-    def overlap(self, other):
+    def negate(self):
         return
 
-    def __eq__(self, other):
-        return type(self) == type(other) and self.dim == other.dim
+    @abc.abstractmethod
+    def overlap(self, o):
+        return
+
+    def __eq__(self, o):
+        return type(self) == type(o) and self.dim == o.dim
 
     def __hash__(self):
         return hash(self.dim.name)
@@ -59,86 +59,88 @@ class NullInterval(AbstractInterval):
     is_Null = True
 
     def __repr__(self):
-        return "NullInterval(%s)" % self.dim
-
-    @property
-    def is_unbounded(self):
-        return False
+        return "%s[Null]" % self.dim
 
     def rebuild(self):
         return NullInterval(self.dim)
 
-    def intersection(self, other):
+    def intersection(self, o):
         return self.rebuild()
 
-    def union(self, other):
-        if self.dim == other.dim:
-            return other.rebuild()
+    def negate(self):
+        return self.rebuild()
+
+    def union(self, o):
+        if self.dim == o.dim:
+            return o.rebuild()
         else:
-            return Box([self.rebuild(), other.rebuild()])
+            return Box([self.rebuild(), o.rebuild()])
 
-    def union_if_overlap(self, other):
-        return self.rebuild()
-
-    def overlap(self, other):
+    def overlap(self, o):
         return False
 
 
 class Interval(AbstractInterval):
 
+    """
+    Interval(dim, lower, upper)
+
+    Create an :class:`Interval` of extent: ::
+
+        dim.extent + abs(upper - lower)
+    """
+
     is_Defined = True
 
-    def __init__(self, dim, lower=None, upper=None):
+    def __init__(self, dim, lower, upper):
+        assert isinstance(lower, int)
+        assert isinstance(upper, int)
         super(Interval, self).__init__(dim)
-        self.lower = lower or -np.inf
-        self.upper = upper or np.inf
-        assert self.lower <= self.upper
+        self.lower = lower
+        self.upper = upper
+        self.min_extent = abs(upper - lower)
+        self.extent = dim.symbolic_size + self.min_extent
 
     def __repr__(self):
-        return "Interval(%s <= %s <= %s)" % (self.lower, self.dim, self.upper)
-
-    @property
-    def is_unbounded(self):
-        return self.lower == -np.inf or self.upper == np.inf
+        return "%s[%s, %s]" % (self.dim, self.lower, self.upper)
 
     def rebuild(self):
         return Interval(self.dim, self.lower, self.upper)
 
-    def intersection(self, other):
-        if self.overlap(other):
-            return Interval(self.dim, max(self.lower, other.lower),
-                            min(self.upper, other.upper))
+    def intersection(self, o):
+        if self.overlap(o):
+            return Interval(self.dim, max(self.lower, o.lower),
+                            min(self.upper, o.upper))
         else:
             return NullInterval(self.dim)
 
-    def union(self, other):
-        if self.overlap(other):
-            return Interval(self.dim, min(self.lower, other.lower),
-                            max(self.upper, other.upper))
-        elif other.is_Null and self.dim == other.dim:
+    def union(self, o):
+        if self.overlap(o):
+            return Interval(self.dim, min(self.lower, o.lower),
+                            max(self.upper, o.upper))
+        elif o.is_Null and self.dim == o.dim:
             return self.rebuild()
         else:
-            return Box([self.rebuild(), other.rebuild()])
+            return Box([self.rebuild(), o.rebuild()])
 
-    def union_if_overlap(self, other):
-        if self.overlap(other):
-            return Interval(self.dim, min(self.lower, other.lower),
-                            max(self.upper, other.upper))
-        else:
-            return self.rebuild()
+    def negate(self):
+        return Interval(self.dim, -self.lower, -self.upper)
 
-    def overlap(self, other):
-        if self.dim != other.dim:
+    def overlap(self, o):
+        if self.dim != o.dim:
             return False
         try:
-            return (self.lower <= other.lower and other.lower <= self.upper) or\
-                (self.lower >= other.lower and self.lower <= other.upper)
+            # In the "worst case scenario" the dimension extent is 0
+            # so we can just neglect it
+            min_extent = max(self.min_extent, o.min_extent)
+            return (self.lower <= o.lower and o.lower <= self.lower + min_extent) or\
+                (self.lower >= o.lower and self.lower <= o.lower + min_extent)
         except AttributeError:
             return False
 
-    def __eq__(self, other):
-        return super(Interval, self).__eq__(other) and\
-            self.lower == other.lower and self.upper == other.upper
+    def __eq__(self, o):
+        return super(Interval, self).__eq__(o) and\
+            self.lower == o.lower and self.upper == o.upper
 
     def __hash__(self):
         return hash((self.dim.name, self.lower, self.upper))
@@ -147,7 +149,7 @@ class Interval(AbstractInterval):
 class Box(object):
 
     """
-    A bag of disjoint Intervals.
+    A bag of :class:`Interval`s.
     """
 
     def __init__(self, intervals):
@@ -160,29 +162,16 @@ class Box(object):
     def __repr__(self):
         return "Box[%s]" % ', '.join([repr(i) for i in self.intervals])
 
-    def __eq__(self, other):
-        return len(self.intervals) == len(other.intervals) and\
-            all(i == j for i, j in zip(self.intervals, other.intervals))
+    def __eq__(self, o):
+        return len(self.intervals) == len(o.intervals) and\
+            all(i == j for i, j in zip(self.intervals, o.intervals))
 
-    @classmethod
-    def union(cls, *boxes):
-        mapper = {}
+    def intersection(self, *boxes):
+        mapper = {i.dim: [i] for i in self.intervals}
         for i in boxes:
             for interval in i.intervals:
-                mapper.setdefault(interval.dim, []).append(interval)
-        return Box([Interval.op(v, 'union') for v in mapper.values()])
+                mapper.get(interval.dim, []).append(interval)
+        return Box([Interval.op(v, 'intersection') for v in mapper.values()])
 
-    def union_if_bounded_overlap(self, *boxes):
-        union_box = Box.union(*boxes)
-        intervals = []
-        for i in self.intervals:
-            if i.is_unbounded:
-                intervals.append(i.rebuild())
-                continue
-            base = i
-            for j in union_box.intervals:
-                if j.is_unbounded:
-                    continue
-                base = base.union_if_overlap(j)
-            intervals.append(base)
-        return Box(intervals)
+    def negate(self):
+        return Box([i.negate() for i in self.intervals])
