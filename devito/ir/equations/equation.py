ceed2458a4b1cc0eb50471883437d8f43dea2c55
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-01-09 15:04:11 +0100

    SQUASH WITH LOWERED EQUATION

--- devito/ir/equations/equation.py
+++ devito/ir/equations/equation.py
@@ -1,6 +1,8 @@
+from collections import OrderedDict
+
 import sympy
 
-from devito.ir.support import Box, Interval, Stencil
+from devito.ir.support import Interval, IterationSpace, Stencil
 from devito.symbolics import dimension_sort, indexify
 
 __all__ = ['Eq']
@@ -9,14 +11,14 @@ __all__ = ['Eq']
 class Eq(sympy.Eq):
 
     """
-    A new SymPy equation with an associated data space.
+    A new SymPy equation with an associated iteration space.
 
     All :class:`Function` objects within ``expr`` get indexified and thus turned
     into objects of type :class:`types.Indexed`.
 
-    A data space is an object of type :class:`Box`. It represents the data points
-    accessed by the equation along each iteration :class:`Dimension`. The iteration
-    :class:`Dimension`s are extracted from the :class:`Indexed`s of the equation.
+    An iteration space is an object of type :class:`Space`. It represents the
+    data points accessed by the equation along each :class:`Dimension`. The
+    :class:`Dimension`s are extracted directly from the equation.
     """
 
     def __new__(cls, input_expr, subs=None):
@@ -33,15 +35,25 @@ class Eq(sympy.Eq):
         expr = super(Eq, cls).__new__(cls, expr.lhs, expr.rhs, evaluate=False)
         expr.is_Increment = getattr(input_expr, 'is_Increment', False)
 
+        # Get the accessed data points
+        stencil = Stencil(expr)
+
         # Well-defined dimension ordering
         ordering = dimension_sort(expr, key=lambda i: not i.is_Time)
-        parents = [d.parent for d in ordering if d.is_Stepping]
-        ordering = [i for i in ordering if i not in parents]
 
-        # Data space derivation
-        stencil = Stencil(expr)
-        expr.dspace = Box([Interval(i, min(stencil.get(i)), max(stencil.get(i)))
-                           for i in ordering])
+        # Split actual Intervals (the data spaces) from the "derived" iterators,
+        # to build an IterationSpace
+        iterators = OrderedDict()
+        for i in ordering:
+            if i.is_Derived:
+                iterators.setdefault(i.parent, []).append(stencil.entry(i))
+            else:
+                iterators.setdefault(i, [])
+        intervals = []
+        for k, v in iterators.items():
+            offs = set.union(set(stencil.get(k)), *[i.ofs for i in v])
+            intervals.append(Interval(k, min(offs), max(offs)).negate())
+        expr.ispace = IterationSpace(intervals, iterators)
 
         return expr
 
